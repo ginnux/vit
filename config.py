4fa36fe3874b3af54e8bc66ce56aca620cd192f1
autodl <coldwarrior@163.com>
2024-06-23 16:53:18 +0800

    train1

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ def get_config():
     config["learning_rate"] = 3e-5
     config["batch_size"] = 64
     config["num_workers"] = 8
-    config["pretrained_model"] = "google/vit-base-patch16-224-in21k"
+    config["pretrained_model"] = "google/vit-large-patch16-224-in21k"
     config["load_last_checkpoint"] = False
-    config["epochs"] = 5
+    config["epochs"] = 3
 
     return config
